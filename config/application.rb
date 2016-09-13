de1e06024d9bda8ab90d7b4bd4d1288f765147a1
ChanneDesign <channebertrand@gmail.com>
2016-09-13 19:30:46 -0400

    fa_icon

--- config/application.rb
+++ config/application.rb
@@ -32,14 +32,15 @@ module Opendrives
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.middleware.insert_before ActionDispatch::Static, Rack::Cors do
-    allow do
-      origins ''
-      resource '',
-      headers: :any,
-      methods: [:get, :options]
+        allow do
+          origins ''
+          resource '',
+          headers: :any,
+          methods: [:get, :options]
+        end
     end
     config.assets.paths << Rails.root.join("app", "assets")
-    config.assets.precompile += %w( .svg .eot .woff .ttf )
+    config.assets.precompile += %w( .svg .eot .woff .ttf .woff2 )
     config.assets.header_rules = {
       :global => {'Cache-Control' => 'public, max-age=31536000'},
       :fonts  => {'Access-Control-Allow-Origin' => '*'}
