e4ffabbcbd619a9368dcb20f3f7dc160c027532f
mrdiv <james42432@aol.com>
2016-09-19 07:14:06 -0700

    Updated Products
    Updated products pages to reflect OpenDrives updated wording

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,21 +3,21 @@ class Product
 	def self.products
 		[
 			[
-				"Software features included with the system at no additional fee",
+				"Enterprise software features at no additional cost",
 				"Industry-standard networking protocols",
 				"No per-seat licensing fee",
 				"Flexible Ethernet architecture that requires no special connection software or hardware"
 			],
 			[
 				"An easy-to-use management interface (OPUS GUI)",
-				"Industry-leading bandwidth monitoring and throttling engine",
+				"Exclusive bandwidth monitoring and throttling engine",
 				"Fault-tolerant storage",
-				"An innovative IOPs balancing engine, so one client operation doesn’t overwhelm all others"
+				"Innovative IOPS balancing engine.  One client operation doesn’t overwhelm all others"
 			],
 			[
-				"Metadata stored in memory at all times",
-				"Inline, with no performance degradation compression",
-				"File system as an Object, allowing you to move entire file systems between OpenDrives products, fully checksummed at line speed",
+				"Advanced metadata management",
+				"Inline lossless compression",
+				"Fully checksummed file system providing unprecedented integrity and protection",
 			]
 		]
 	end
